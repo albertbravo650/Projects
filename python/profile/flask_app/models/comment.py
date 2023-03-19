f11db93dd4e5056159f08504cd87dc1a8c9318bc
Albert B <abduarte20@gmail.com>
2023-03-19 00:01:53 -0700

    next commit

--- python/profile/flask_app/models/comment.py
+++ python/profile/flask_app/models/comment.py
@@ -11,3 +11,17 @@ class Comment:
         self.textLink = data['textLink']
         self.createdAt = data['createdAt']
         self.updatedAt = data['updatedAt']
+
+    @classmethod
+    def save(cls, data):
+        query = '''INSERT INTO comments 
+        ( text,  textLink, created_at, updated_at ) 
+        VALUES ( %(text)s , %(textLink)s, NOW() , NOW() );'''
+        return connectToMySQL(mydb).query_db( query, data )
+
+    @classmethod
+    def getById(cls, data):
+        query = " SELECT * FROM comments WHERE id = %(id)s;"
+        results = connectToMySQL(mydb).query_db( query, data )
+        print(results)
+        return cls(results[0])
