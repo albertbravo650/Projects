88842518069c19ac8b8cf66b4f0d58baf3c92aca
Albert B <abduarte20@gmail.com>
2023-03-21 00:48:34 -0700

    next commit

--- python/profile/flask_app/controllers/nextcontroller.py
+++ python/profile/flask_app/controllers/nextcontroller.py
@@ -1,7 +1,7 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
 from flask_app.models.user import User
 from flask_app.models import comment
-from flask_app import app
-from flask import render_template, redirect, request, session, flash
 
 @app.route('/dashboard')
 def dashboard():
@@ -12,7 +12,7 @@ def dashboard():
     }
     # users_players = User.get_user_with_players(data)
     # return render_template('dashboard.html', user=User.getById(data), users_players=users_players)
-    return render_template('home.html')
+    return render_template('home.html', user=User.getById(data))
 
 # @app.route('/add')
 # def new_player():
