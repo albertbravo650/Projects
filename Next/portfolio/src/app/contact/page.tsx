8fdcc744be9d0d0d230c23f7db2051d4a05070c0
Albert B <abduarte20@gmail.com>
2024-02-25 21:42:13 -0800

    next commit

--- Next/portfolio/src/app/contact/page.tsx
+++ Next/portfolio/src/app/contact/page.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import Card from '../components/Card/Card'
+import Link from 'next/link';
+import './contact.css'
 
 export default function Contact() {
     return (
         <div className='mid'>
             <div className="hero-text">
                 <h1 className="text-4xl font-bold mt-4">Contact</h1>
-                <p></p>
+                <div className='info'>
+                    <p className='mb-5'>Email:<span className='ml-5'>abduarte20@gmail.com</span></p>
+                    <p className='mb-5'>LinkedIn:
+                        <Link href={'https://www.linkedin.com/in/albertbravo/'}>
+                            <button className="contactButton ml-5" data-text="LinkedIn">click to visit my LinkedIn</button>
+                        </Link>
+                    </p>
+                    <p className='mb-5'>GitHub:
+                        <Link href={'https://www.linkedin.com/in/albertbravo/'}>
+                            <button className="contactButton ml-5" data-text="LinkedIn">click to visit my GitHub</button>
+                        </Link>
+                    </p>
+                </div>
             </div>
-            
         </div>
     )
 }
