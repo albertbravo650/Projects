7044e985ef44d0a7bd2f2d000643ac75ca559f45
Albert Bravo PC <abduarte20@gmail.com>
2024-10-30 19:13:44 -0700

    next commit

---
+++ JS/Practice/primitives.js
@@ -0,0 +1,19 @@
+// let name = "Albert Bravo";
+// let age = 28;
+
+// console.log(`My name is ${name} and I am ${age} year(s) old.`);
+
+// if(age || age === 0) {
+//     console.log(`The value of the age variable is ${age}.`);
+// } else {
+//     console.log("Age does not exist.");
+// }
+
+// let testBool = !!false;
+// console.log(testBool);
+
+let fName = Symbol('First Name');
+let lName = Symbol('Last Name');
+
+console.log(fName);
+console.log(lName);
